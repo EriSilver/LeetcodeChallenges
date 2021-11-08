a4d8ae656ebba04aac8850b34de641ed4de4d769
silver <dr.george.erini@gmail.com>
2021-11-08 06:04:47 +0200

    14/15 passed -- still a lil slow for the last test

--- range-sum-query-mutable/code.py
+++ range-sum-query-mutable/code.py
@@ -1,19 +1,59 @@
 class NumArray(object):
+
     def __init__(self, nums):
         """
         :type nums: List[int]
         """
         self.nums = nums
-
+        
+        self.sums = []
+        
+        i = 0
+        self.numsLen = len(nums)
+        theInt = self.numsLen // 20
+        theFloat = self.numsLen / 20
+        
+        for i in range((theInt + 1 if theFloat > theInt else theInt) + 1):
+            start = i * 20
+            end = (i + 1) * 20
+            if end > self.numsLen:
+                end = self.numsLen
+            self.sums += [sum(nums[start  : end])]
+            
     def update(self, index, val):
         """
         :type index: int
         :type val: int
         :rtype: None
         """
+        
+        self.sums[index//20] += (val - self.nums[index])
         self.nums[index] = val
         
 
+    #######################################################
+    def extremity(self, left, right):
+        
+        # right excluded 
+        if right - left > 10:
+            count = 0
+            partition = left // 20
+            numsIndex = partition * 20
+            # exclude the sum of the extremities 
+            #right is wanted - left to be excluded 
+            if numsIndex < left:
+                count += (self.sums[partition] - sum(self.nums[numsIndex: left]))
+            else: 
+                #left is wanted - right to be excluded
+                numsIndex += 20
+                if numsIndex > self.numsLen:
+                      numsIndex = self.numsLen
+                count += (self.sums[partition] - sum(self.nums[right: numsIndex]))
+            return count
+        else:
+            return sum(self.nums[left:right])
+    
+    #######################################################
     def sumRange(self, left, right):
         """
         :type left: int
@@ -21,11 +61,26 @@ class NumArray(object):
         :rtype: int
         """
         count = 0
-        while left <= right:
-            count += self.nums[left]
-            left += 1
-        return count
+        isSamePartition = left // 20 == right // 20
+        # the left extremity |------#######|######
+        # the right part is wanted and left to be excluded
+        lastLeft = (20 - (left % 20)) + left
+        count += self.extremity(left, lastLeft if not isSamePartition else right + 1)
         
+        if not isSamePartition:
+            # the right extremity #####|#######------|
+            # the left part is wanted and right to be excluded
+            firstRight = right - (right % 20)
+            count += self.extremity(firstRight if not isSamePartition else left, right + 1)  
+            
+            start = lastLeft // 20
+            end = firstRight // 20 
+            while start < end:
+                count += self.sums[start]
+                start += 1
+                          
+        return count
+                        
 
 
 # Your NumArray object will be instantiated and called as such:
