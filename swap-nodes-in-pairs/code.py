356046148cf92c1c347823c640106ce158f388e6
silver <dr.george.erini@gmail.com>
2021-12-09 15:17:33 +0200

    MEDIUM - Runtime: 20 ms, faster than 61.50% of Python submissions

---
+++ swap-nodes-in-pairs/code.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        one = head
+        prev = None
+        if one and one.next:
+            head = one.next
+            while one and one.next:
+                two = one.next
+                three = two.next
+
+                tmp = one
+                one.next = two.next
+                two.next = tmp
+
+                if prev:
+                    prev.next = two
+
+                if one:
+                    prev = one
+                    one = one.next
+
+        return head
+
+"""
+ATT TIME:
+Accepted: 727,972
+Submissions: 1,293,441
+
+Runtime: 20 ms, faster than 61.50% of Python online submissions for Swap Nodes in Pairs.
+Memory Usage: 13.3 MB, less than 85.95% of Python online submissions for Swap Nodes in Pairs.
+"""
