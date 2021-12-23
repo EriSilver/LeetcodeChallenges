8a78b931e3525321a37b8283395569537e5ce013
silver <dr.george.erini@gmail.com>
2021-12-24 00:54:57 +0200

    297 / 304 test cases passed

--- powx-n/code.py
+++ powx-n/code.py
@@ -1,13 +1,14 @@
 class Solution(object):
-    def smallN(self, n, div=0):
-        x = 10
-        while (n % x) > 0 and x > 1:
-            x -= 1
+    def smallN(self, n, div=0, x=0):
+        if not x:
+            x = 10
+            while (n % x) > 0 and x > 1:
+                x -= 1
             
-        if n < 1000 or x == 1:
-            return n, div
-        
-        return self.smallN(n/x, div+x)
+        if n < 1000 or x == 1 or (x and (n % x) > 0):
+            return n, div 
+
+        return self.smallN(n/x , div + x, x)
         
     def myPow(self, x, n):
         """
@@ -44,6 +45,8 @@ class Solution(object):
 '''
     Modified for big nums of n
 
+    the problem is adding 2 divs (second recursion in smallN)
+
     Failed here: 
     x = 1.00001
     n = 123456
@@ -57,6 +60,8 @@ test examples:
 1.00001
 123456
 
+1.00012
+20502
 1.00012
 1024
 2.00000
